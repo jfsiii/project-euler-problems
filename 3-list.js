ca57e68bb2670cbefbe703cc6b273c04025b0478
John Schulz <JFSIII@JFSIII.org>
2012-02-04 15:38:20 -0500

    Not "functional" per se, but suitable for list based programming

---
+++ 3-list.js
@@ -0,0 +1,28 @@
+/*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the num 600851475143 ?
+ *
+ * From http://projecteuler.net/problem=3
+ */
+(function (num) {
+
+function primeFactorsOf(num) {
+	var i = 2;
+	var primeFactors = [];
+
+	for (; i <= num; i++) {
+        if (num % i === 0) {
+			primeFactors.push(i);
+            num /= i--;
+        }
+    }
+
+    return primeFactors;
+}
+
+var primeFactors = primeFactorsOf(num);
+var largest = Math.max.apply(Math, primeFactors);
+console.log(largest);
+
+})(600851475143);

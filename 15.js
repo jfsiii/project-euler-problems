f98995c7efef50aa81bc88d643ef1da6f1420510
John Schulz <JFSIII@JFSIII.org>
2012-02-27 09:32:03 -0500

    Solved

--- 15.js
+++ 15.js
@@ -5,8 +5,33 @@
  *
  * From http://projecteuler.net/problem=15
  */
-(function (x, y) {
-	// theories:
-	// * based on navigable sides (12 in a 2 x 2)
-	// * based on x^y +/- some multiple of a side?
-})(2, 2);
+(function (m, n) {
+
+    var sum = m + n;
+    var sum_2 = sum / 2;
+    var factorial_sum_2 = factorial(sum_2);
+    var routes = factorial(sum) / (factorial_sum_2 * factorial_sum_2);
+
+    console.log(routes, 'routes in a', m, 'x', n, 'grid');
+    return routes;
+
+    // internal functions
+    function Range(min, max) {
+        var range = [];
+        var i = min;
+        for (; i <= max; ++i) {
+            range.push(i);
+        }
+
+        return range;
+    }
+
+    function factorial(n) {
+        return (Range(1, n)).reduce(multiply);
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+})(20, 20);
